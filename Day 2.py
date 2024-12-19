466bd3fafa7294491ac8458a0acb913226ab1197
Celestial Blaze <152753428+Celestial-Blaze@users.noreply.github.com>
2024-12-18 20:26:52 -0500

    hellish success and failure in day 2

--- Day 2.py
+++ Day 2.py
@@ -13,16 +13,96 @@ reports = []
 
 for line in day_2_input_list:
     reports.append(line.strip().split(" "))
-    # TODO: make sure you turn values to ints before processing
 
 safe_reports = 0  # counter for how many reports are safe
 
+# okay so for part 2 I'm going with the greedy approach and removing the first problem I encounter instead of
+# checking all configurations of the sequence with one less element to see which ones are safe because that
+# would be a factorial scale problem -- and I know that based on the size of the data, Python won't even complain
+# and execute it just fine, but I really don't want to write such terrifying code unless I really have to
 for report in reports:
-    report.sort()  # all hail the mighty TimSort
+    print(f"checking for report: {report}")
     safe_flag = True
     level_index = 0
-    while safe_flag and level_index < len(report):
-        # increasing or decreasing sequence constraint
-        # 1 <= difference between consecutive levels <= 3 constraint
-        if report[level_index] == report[level_index + 1]:
+    pop_count = 0
+    # choose increasing or decreasing sequence constraint
+    if int(report[0]) < int(report[1]):  # increasing sequence route
+        print("it might be an increasing sequence, let's find out")
+        while level_index < len(report) - 1:
+            # 1 <= difference between consecutive levels <= 3 constraint
+            if (not (1 <= abs(int(report[level_index]) - int(report[level_index + 1])) <= 3)) \
+                    or int(report[level_index]) >= int(report[level_index + 1]):
+                if pop_count < 1:  # if we can fix the problem, fix it!
+                    print("first error! fixing now")
+                    report.pop(level_index)  # pop the current item, the one causing the issue and then...
+                    pop_count += 1  # increment pop_count
+                    continue
+                else:  # if we can't, then oh well
+                    print(f"BROKE RULES AFTER {report[level_index]}")
+                    safe_flag = False
+                    break
+            else:
+                print(f"so far so good... we're at {report[level_index]}")
+                level_index += 1
+
+    elif int(report[0]) > int(report[1]):  # decreasing sequence route
+        print("it might be a decreasing sequence, let's find out")
+        while level_index < len(report) - 1:
+            # 1 <= difference between consecutive levels <= 3 constraint
+            if (not (1 <= abs(int(report[level_index]) - int(report[level_index + 1])) <= 3)) \
+                    or int(report[level_index]) <= int(report[level_index + 1]):
+                if pop_count < 1:  # if we can fix the problem, fix it!
+                    print("first error! fixing now")
+                    report.pop(level_index)  # pop the current item, the one causing the issue and then...
+                    pop_count += 1  # increment pop_count
+                    continue
+                else:  # if we can't, then oh well
+                    print(f"BROKE RULES AFTER {report[level_index]}")
+                    safe_flag = False
+                    break
+            else:
+                print(f"so far so good... we're at {report[level_index]}")
+                level_index += 1
+
+    else:
+        # case when first two elements are equal
+        print("first two elements match so I'm deleting the second one")
+        report.pop(level_index + 1)  # pop the next item, the one causing the issue and then...
+        pop_count += 1  # not that it matters here
+        # choose increasing or decreasing sequence constraint
+        if int(report[0]) < int(report[1]):  # increasing sequence route
+            print("it might be an increasing sequence now, let's find out")
+            while level_index < len(report) - 1:
+                # 1 <= difference between consecutive levels <= 3 constraint
+                if (not (1 <= abs(int(report[level_index]) - int(report[level_index + 1])) <= 3)) \
+                        or int(report[level_index]) >= int(report[level_index + 1]):
+                    print(f"BROKE RULES AFTER {report[level_index]}")
+                    safe_flag = False
+                    break
+                else:
+                    print(f"so far so good... we're at {report[level_index]}")
+                    level_index += 1
+        elif int(report[0]) > int(report[1]):  # decreasing sequence route
+            print("it might be a decreasing sequence now, let's find out")
+            while level_index < len(report) - 1:
+                # 1 <= difference between consecutive levels <= 3 constraint
+                if (not (1 <= abs(int(report[level_index]) - int(report[level_index + 1])) <= 3)) \
+                        or int(report[level_index]) <= int(report[level_index + 1]):
+                    print(f"BROKE RULES AFTER {report[level_index]}")
+                    safe_flag = False
+                    break
+                else:
+                    print(f"so far so good... we're at {report[level_index]}")
+                    level_index += 1
+        else:
+            # wow, same issue again? what...? more doubles? FINE, NOT SAFE
             safe_flag = False
+    # outcome
+    if safe_flag:
+        print("SAFE")
+        safe_reports += 1
+    print("----------------------------------------------------------------------------------------------")
+
+print(safe_reports)
+
+# AAAAAAHHHHHHHHHH I DON'T WANNA BRUTE FORCE (┬┬﹏┬┬)
---
+++ test.py
@@ -0,0 +1,1 @@
+# for testing silly little python thingies cause my brain's not enough for debugging
